4788e381a80049a03770c47ff4749da7191d0c12
Eliott Bovel <eliottbovel.pro@gmail.com>
2019-01-23 16:41:22 +0100

    Delete Inutile File.

--- src/main/java/fr/eliottbovel/tchatcontroller/Command/Sub/TCSubPlayer.java
+++
@@ -1,9 +0,0 @@
-package fr.eliottbovel.tchatcontroller.Command.Sub;
-
-import org.bukkit.entity.Player;
-
-public class TCSubPlayer {
-    public TCSubPlayer(Player player, String[] args) {
-
-    }
-}
